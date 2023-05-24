71905b317f31d9b269360deee167dca14e079c63
ClementeB <clemente.barros.b@gmail.com>
2023-05-24 10:08:18 -0400

    cordenadas polares

--- clase matplotlib.py
+++ clase matplotlib.py
@@ -7,10 +7,19 @@ plt.plot(x,y)
 plt.show()
 
 
-
+#grafico lineal
 x= np.arange(0,10)
 plt.plot(x,np.sin(x))
 plt.show()
 
+
+#grafico de barras
 plt.bar([1,2,3,4,5,6],[1,3,3,9,8,6])
+plt.show()
+
+#cordenadas polares
+r= np.linspace(0,2*np.pi,500)
+t=5*r
+
+plt.polar(t,r)
 plt.show()
