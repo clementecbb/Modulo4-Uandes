7c010cde20fbb0386fd916eb98538eef836fa757
ClementeB <clemente.barros.b@gmail.com>
2023-05-24 09:53:37 -0400

    first commit

---
+++ clase array.py
@@ -0,0 +1,28 @@
+import numpy as np
+
+
+#array
+array= np.array([1,2,3,4,5,6,7,8,9,10])
+print(array)
+
+
+#array de 0
+z=np.zeros((10,5))
+print(z)
+
+
+#array de 1
+u= np.ones((10,5))
+print(u)
+
+
+#array de random
+r=np.random.random((10,5))
+print(r)
+
+
+#array del 0 al 10 con 100 valores entremedio
+ls= np.linspace(0,10,100)
+print(ls)
+
+
---
+++ clase matplotlib.py
@@ -0,0 +1,13 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+y=[1,2,5,4,7,3,7,9,10]
+x=[3,4,2,6,8,3,6,5,10]
+plt.plot(x,y)
+plt.show()
+
+
+
+x= np.arange(0,10)
+plt.plot(x,np.sin(x))
+plt.show()
